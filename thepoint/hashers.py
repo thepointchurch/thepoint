a9219257dcfa621c464ad8eb4e8b1b2ea9505dff
Craig Holyoak <craig@helmsdeep.org>
2022-11-25 10:48:46 +1000

    Dependency updates

--- thepoint/hashers.py
+++ thepoint/hashers.py
@@ -2,5 +2,4 @@ from django.contrib.auth.hashers import Argon2PasswordHasher
 
 
 class LiteArgon2PasswordHasher(Argon2PasswordHasher):  # pylint: disable=too-few-public-methods
-    memory_cost = 51200
     parallelism = 1
